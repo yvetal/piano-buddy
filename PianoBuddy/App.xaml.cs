d3420a6dc65092173e067dd6535a008d84ea9ce4
Neel <neel.pendharkar@gmail.com>
2025-10-24 19:12:44 +0530

    Empty Webapp #1

---
+++ PianoBuddy/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PianoBuddy
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
