0b164b9d858bd61ec9290fb477e45ff3ad3c9c89
Neel <neel.pendharkar@gmail.com>
2025-10-25 19:18:34 +0530

    Midi Device Provider #10

---
+++ PianoBuddy.Tests/MidiDeviceFinderTests.cs
@@ -0,0 +1,27 @@
+﻿using Moq;
+using PianoBuddy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoBuddy.Tests
+{
+    public class MidiDeviceFinderTests
+    {
+        [Fact]
+        public void TestFindDevices()
+        {
+            var mockProvider = new Mock<IMidiDeviceProvider>();
+            mockProvider.Setup(p => p.GetDeviceCount()).Returns(2);
+            mockProvider.Setup(p => p.GetDeviceName(0)).Returns("Keyboard");
+            mockProvider.Setup(p => p.GetDeviceName(1)).Returns("DrumPad");
+
+            var detector = new MidiDeviceFinder(mockProvider.Object);
+            var devices = detector.FindDevices();   
+
+            Assert.Equal("MIDI Devices:\n0: Keyboard\n1: DrumPad\n", devices);
+        }
+    }
+}
---
+++ PianoBuddy/Services/MidiDeviceFinder.cs
@@ -0,0 +1,39 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoBuddy.Services
+{
+    public interface IMidiDeviceProvider
+    {
+        string GetDeviceName(int index);
+        int GetDeviceCount();
+    }
+
+    public class MidiDeviceProvider : IMidiDeviceProvider
+    {
+        public string GetDeviceName(int index) => MidiIn.DeviceInfo(index).ProductName;
+        public int GetDeviceCount() => MidiIn.NumberOfDevices;
+    }
+    public class MidiDeviceFinder
+    {
+        private readonly IMidiDeviceProvider _deviceProvider;
+        public MidiDeviceFinder(IMidiDeviceProvider deviceProvider)
+        {
+            _deviceProvider = deviceProvider;
+        }
+        public string FindDevices()
+        {
+            string deviceList = "MIDI Devices:\n";
+            int deviceCount = _deviceProvider.GetDeviceCount();
+            for (int i = 0; i < deviceCount; i++)
+            {
+                deviceList += $"{i}: {_deviceProvider.GetDeviceName(i)}\n";
+            }
+            return deviceList;
+        }
+    }
+}
