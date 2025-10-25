6b3b3ae578921f0cc559c7d2f3ea5c971da9d3dd
Neel <neel.pendharkar@gmail.com>
2025-10-25 20:48:36 +0530

    New MidiService Class #12

---
+++ PianoBuddy.Tests/MidiServiceTests.cs
@@ -0,0 +1,86 @@
+﻿using NAudio.Midi;
+using PianoBuddy.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoBuddy.Tests
+{
+    public class FakeMidiIn : IMidiIn
+    {
+        public event EventHandler<MidiInMessageEventArgs> MessageReceived = delegate { };
+        public bool Started { get; private set; } = false;
+        public bool Disposed { get; private set; }
+
+        public int Index { get; }
+
+        public FakeMidiIn(int index = 0)
+        {
+            Index = index;
+        }
+        public void Start() => Started = true;
+        public void Stop() => Started = false;
+        public void Dispose() => Disposed = true;
+        public void SimulateMessage(MidiInMessageEventArgs e)
+        {
+            MessageReceived?.Invoke(this, e);
+        }
+    }
+    public class MidiServiceTests
+    {
+
+        [Fact]
+        public void TestStart()
+        {
+            var fakeDevice = new FakeMidiIn(0);
+            var service = new MidiService(index => fakeDevice);
+            
+            service.Start(0);
+
+            Assert.True(fakeDevice.Started);
+            Assert.Equal(0, fakeDevice.Index);
+
+        }
+        [Fact]
+        public void NoteReceived_EventIsRaised_WhenMidiMessageArrives()
+        {
+            var fakeDevice = new FakeMidiIn(0);
+            var service = new MidiService(index => fakeDevice);
+            
+            NoteEvent receivedNote = null;
+            service.NoteReceived += (s, e) => receivedNote = e;
+
+            service.Start(0);
+
+            var noteOn = new NoteOnEvent(0, 1, 64, 127, 0);
+            int raw = noteOn.GetAsShortMessage();
+            var message = new MidiInMessageEventArgs(raw, 0);
+
+            fakeDevice.SimulateMessage(message);
+
+            Assert.NotNull(receivedNote);
+
+            Assert.Equal(noteOn.NoteNumber, receivedNote.NoteNumber);
+            Assert.Equal(noteOn.Velocity, receivedNote.Velocity);
+            Assert.Equal(noteOn.Channel, receivedNote.Channel);
+            Assert.Equal(noteOn.CommandCode, receivedNote.CommandCode);
+        }
+        [Fact]
+        public void Stop_ShouldStopAndDisposeMidiDevice()
+        {
+            // Arrange
+            var fakeDevice = new FakeMidiIn(0);
+            var service = new MidiService(i => fakeDevice);
+            service.Start(0);
+
+            // Act
+            service.Stop();
+
+            // Assert
+            Assert.False(fakeDevice.Started);    // Stop() sets Started to false
+            Assert.True(fakeDevice.Disposed);    // Dispose() should have been called
+        }
+    }
+}
---
+++ PianoBuddy/Services/MidiService.cs
@@ -0,0 +1,67 @@
+﻿using NAudio.Midi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PianoBuddy.Services
+{
+    public interface IMidiIn
+    {
+        event EventHandler<MidiInMessageEventArgs> MessageReceived;
+        void Start();
+        void Stop();
+        void Dispose();
+    }
+    public class MidiInWrapper : IMidiIn
+    {
+        private readonly MidiIn _midiIn;
+
+        public event EventHandler<MidiInMessageEventArgs> MessageReceived
+        {
+            add { _midiIn.MessageReceived += value; }
+            remove { _midiIn.MessageReceived -= value; }
+        }
+
+        public MidiInWrapper(int deviceIndex)
+        {
+            _midiIn = new MidiIn(deviceIndex);
+        }
+
+        public void Start() => _midiIn.Start();
+        public void Stop() => _midiIn.Stop();
+        public void Dispose() => _midiIn.Dispose();
+    }
+    public class MidiService
+    {
+        private readonly Func<int, IMidiIn> _midiInFactory;
+        private IMidiIn _midiIn;
+        public event EventHandler<NoteEvent> NoteReceived;
+
+        public MidiService(Func<int, IMidiIn> midiInFactory)
+        {
+            _midiInFactory = midiInFactory;
+        }
+        public void Start(int deviceIndex)
+        {
+            _midiIn = _midiInFactory(deviceIndex);
+            _midiIn.MessageReceived += OnMessageReceived;
+            _midiIn.Start();
+        }
+        public void Stop()
+        {
+            _midiIn?.Stop();
+            _midiIn?.Dispose();
+        }
+        private void OnMessageReceived(object sender, MidiInMessageEventArgs e)
+        {
+            var midiEvent = MidiEvent.FromRawMessage(e.RawMessage);
+
+            if (midiEvent is NoteEvent noteEvent)
+            {
+                NoteReceived?.Invoke(this, noteEvent);
+            }
+        }
+    }
+}
