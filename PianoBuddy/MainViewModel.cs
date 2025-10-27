580cacd9df202badab38dd371beb4e3839f4474b
Neel <neel.pendharkar@gmail.com>
2025-10-27 23:13:39 +0530

    Make ViewModel the datacontext #17

--- PianoBuddy/MainViewModel.cs
+++ PianoBuddy/MainViewModel.cs
@@ -1,36 +1,64 @@
-﻿using System.ComponentModel;
+﻿using NAudio.Midi;
+using PianoBuddy.Services;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace PianoBuddy
 {
     public class MainViewModel : INotifyPropertyChanged
     {
-        private string _devices;
-        private string _currentNote;
+        private readonly MidiDeviceFinder _deviceFinder;
+        private readonly MidiService _midiService;
+        private readonly SynchronizationContext _uiContext;
 
+        private string _devices;
         public string Devices
         {
             get => _devices;
-            set
-            {
-                _devices = value;
-                OnPropertyChanged(nameof(Devices));
-            }
+            set { _devices = value; OnPropertyChanged(); }
         }
 
+        private string _currentNote = "No note played yet";
         public string CurrentNote
         {
             get => _currentNote;
-            set
+            set { _currentNote = value; OnPropertyChanged(); }
+        }
+
+        public MainViewModel()
+        {
+            _uiContext = SynchronizationContext.Current ?? new SynchronizationContext();
+
+            var provider = new MidiDeviceProvider();
+            _deviceFinder = new MidiDeviceFinder(provider);
+            _midiService = new MidiService(i => new MidiInWrapper(i));
+
+            _midiService.NoteReceived += OnNoteReceived;
+
+            DetectMidiDevices();
+            StartFirstMidiDevice();
+        }
+
+        private void DetectMidiDevices()
+        {
+            Devices = _deviceFinder.FindDevices();
+        }
+
+        private void StartFirstMidiDevice()
+        {
+            _midiService.Start(0);
+        }
+
+        private void OnNoteReceived(object sender, NoteEvent note)
+        {
+            _uiContext.Post(_ =>
             {
-                _currentNote = value;
-                OnPropertyChanged(nameof(CurrentNote));
-            }
+                CurrentNote = $"Note: {note.NoteName} ({note.NoteNumber})";
+            }, null);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
--- PianoBuddy/MainWindow.xaml.cs
+++ PianoBuddy/MainWindow.xaml.cs
@@ -8,43 +8,9 @@ namespace PianoBuddy
 {
     public partial class MainWindow : Window
     {
-
-        private readonly MainViewModel _vm;
-
-        private MidiDeviceFinder _deviceFinder;
-        private MidiService _midiService;
-
         public MainWindow()
         {
-            InitializeComponent(); 
-            _vm = new MainViewModel();
-            DataContext = _vm;
-            
-            DetectMidiDevices();
-            StartFirstMidiDevice();
-        }
-
-        private void DetectMidiDevices()
-        {
-            var provider = new MidiDeviceProvider();
-            _deviceFinder = new MidiDeviceFinder(provider);
-
-            _vm.Devices = _deviceFinder.FindDevices();
-        }
-
-        private void StartFirstMidiDevice()
-        {
-            _midiService = new MidiService(i => new MidiInWrapper(i)); // Wrap NAudio MidiIn
-            _midiService.NoteReceived += OnNoteReceived;
-            _midiService.Start(0);
-        }
-
-        private void OnNoteReceived(object sender, NoteEvent note)
-        {
-            Dispatcher.Invoke(() =>
-            {
-                _vm.CurrentNote = $"Note: {note.NoteName} ({note.NoteNumber})";
-            });
+            InitializeComponent();
         }
     }
 }
